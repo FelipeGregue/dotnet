f2e3db081b80884dc5ca39ff98eac5789e75e13f
Felipe Gregue <felipe_gregue@hotmail.com>
2023-11-07 17:26:28 -0300

    Primeira Aula Dotnet

---
+++ Semana1/MeuProjeto/Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+#region TiposDados
+int tipointeiro;
+double tipodouble;
+decimal tipodecimal;
+long tipolong;
+float tipofloat;
+
+tipointeiro = int.MaxValue;
+Console.WriteLine("O maior valor inteiro é: " + tipointeiro);
+tipointeiro = int.MinValue;
+Console.WriteLine("O menor valor inteiro é: " + tipointeiro);
+tipolong = long.MaxValue;
+Console.WriteLine("O maior valor long é: " + tipolong);
+tipolong = long.MinValue;
+Console.WriteLine("O menor valor long é: " + tipolong);
+
+#endregion
