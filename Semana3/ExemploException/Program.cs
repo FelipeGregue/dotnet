e501dc91ea49de596b1272b3639e63d84e0c8b3a
FelipeGregue <felipe_gregue@hotmail.com>
2023-11-21 17:10:39 -0300

    Adicionando Outros Testes

---
+++ Semana3/ExemploException/Program.cs
@@ -0,0 +1,26 @@
+﻿#region Exceptions Example
+
+try{
+    int result = Divide(10,0);
+    Console.WriteLine($"Result: {result}");
+}
+catch (DivideByZeroException ex){
+    Console.WriteLine("Erro: Cannot divide by zero");
+    Console.WriteLine(ex.Message);
+}
+catch(Exception ex){
+    Console.WriteLine("An error occurred");
+    Console.WriteLine(ex.Message);
+}
+finally{
+    Console.WriteLine("Finally block executed");
+}
+
+int Divide(int a, int b){
+    if(b == 0)
+    {
+        throw new DivideByZeroException("Cannot divide by zero");
+    }
+    return a / b;
+}
+#endregion
